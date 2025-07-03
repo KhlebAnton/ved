6c23f52c46fda6964060f4104e8857772e2b78d0
Anton Khleb <khleb.4nton@yandex.ru>
2025-07-03 17:52:37 +0500

    fix

--- scripts/header.js
+++ scripts/header.js
@@ -3,12 +3,24 @@ const closeMenuBtn = document.querySelector('.m-menu-close');
 const menuMobile = document.querySelector('.l-menu-mobile');
 const menuMobileContent = document.querySelector('.m-menu');
 
-humburgerBtn.addEventListener('click', ()=> {
+humburgerBtn.addEventListener('click', openMenu);
+
+closeMenuBtn.addEventListener('click', closeMenu);
+function openMenu() {
     menuMobile.classList.add('is-open');
     menuMobileContent.classList.add('is-open');
-});
-
-closeMenuBtn.addEventListener('click', ()=> {
+    toggleNoScroll()
+}
+function closeMenu() {
     menuMobile.classList.remove('is-open');
     menuMobileContent.classList.remove('is-open');
-});
+    toggleNoScroll()
+}
+
+function toggleNoScroll() {
+    const body = document.body;
+    const html = document.documentElement;
+
+    body.classList.toggle('is-no-scrolled');
+    html.classList.toggle('is-no-scrolled');
+}
